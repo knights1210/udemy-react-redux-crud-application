06e22a66ea2acac253e49548773f9a00755bd36c
knights1210 <jamis.1984@gmail.com>
2020-12-06 14:45:08 +0900

    tes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
+import React from 'react';
 
-function App() {
-  return (
-    <div><h1>Hello,world!</h1></div>
-  );
+class App extends Component {
+  render() {
+    <div>Hello,world!</div>
+  
+  }
 }
-
 export default App;
