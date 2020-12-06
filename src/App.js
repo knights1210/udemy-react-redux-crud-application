1696219923a3a17926d0a7d35750ec727fd6dd23
knights1210 <jamis.1984@gmail.com>
2020-12-06 15:09:32 +0900

    why-jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    <div>Hello,world!</div>
-  
+    return <h1>Hello,world!</h1>
+    
   }
 }
+
 export default App;
